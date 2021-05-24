22d56ef67426e95b308cbda38ceca1d4104c7dd6
lua <3159223897@qq.com>
2021-05-24 12:48:00 +0800

    完成了登录和注册功能的开发

---
+++ assets/js/login.js
@@ -0,0 +1,74 @@
+$(function() {
+    // 点击注册账号链接事件
+    $('#link_reg').on('click', function() {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+
+    // 点击登录链接事件
+    $('#link_login').on('click', function() {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+
+    // 从layui中获取form对象
+    var form = layui.form;
+    var layer = layui.layer;
+    // 通过form。verify()函数自定义校验规则
+    form.verify({
+        // 自定义了一个叫做 pwd 的校验规则
+        pwd: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ],
+        // 确认密码校验规则
+        repwd: function(value) {
+            // 通过形参拿到确认密码框的内容
+            // 还需要拿到密码框的内容
+            var pwd = $('.reg-box [name=password]').val()
+                //    判断两个值是否一致
+            if (pwd !== value) {
+                return '两次输入不一致'
+            }
+        }
+    })
+
+    //监听注册表单的提交事件
+    $('#form_reg').on('submit', function(e) {
+        // 1. 阻止默认的提交行为
+        e.preventDefault()
+            // 2. 发起Ajax的POST请求
+        $.post('/api/reguser', { username: $('#form_reg [name=password]').val() }, function(res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功')
+                // 注册成功后跳转到登录页面
+                // 模拟人的点击行为
+            $('#link_login').click()
+        })
+    })
+
+    // 监听登录表单的提交事件
+    $('#form_login').submit(function(e) {
+        // 阻止默认提交行为
+        e.preventDefault();
+        // 2. 发起Ajax的POST请求
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            // 快速获取表单中的数据
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功！')
+                    // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                localStorage.setItem('token', res.token)
+                    // 跳转到后台主页
+                location.href = '/index.html'
+            }
+        })
+    })
+
+})
